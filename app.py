ec760b82db68b57f1c1776d96fcf2fe3375ef322
isaac <i.hodgso01@pudseygrangefield.co.uk>
2019-10-17 13:42:00 +0100

    v0.2
    added age input a check

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 print('hello world')
+
 name = input('what is your name?: ')
+
 print(f'hello {name}')
+
+age = int(input('what is your age: '))
+
+if age >= 18:
+	print('you are an adult')
+else:
+	print('you are a child')
